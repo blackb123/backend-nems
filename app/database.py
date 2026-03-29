0d3b6f31fbad6c3346deb67af9c0dcd304836338
bradmnlv <b.djeutou@mnlvm.com>
2026-03-29 04:12:46 -1100

    Use env variable for database URL

--- app/database.py
+++ app/database.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import sessionmaker
 # ---- Import your models ----
 from app.models.user import Base as UserBase
 from app.models.product import Base as ProductBase
-
-# ---- Database URL for Render ----
-DATABASE_URL = "postgresql+asyncpg://nemsbd_user:HLo20JYbBhL2b10Er4YUfTmo4yPJ5HTo@nemsbd.postgres.render.com:5432/nemsbd"
+from app.core.config import settings
 
 # ---- SSL setup for Render ----
 ssl_context = ssl.create_default_context()
@@ -17,7 +15,7 @@ ssl_context.verify_mode = ssl.CERT_NONE  # Needed for Render's self-signed cert
 
 # ---- Async engine ----
 engine = create_async_engine(
-    DATABASE_URL,
+    settings.database_url,
     connect_args={"ssl": ssl_context},  # Pass SSL only here
     echo=True,  # optional: logs SQL queries
 )
